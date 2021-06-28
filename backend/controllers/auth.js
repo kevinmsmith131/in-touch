2ab533e69223bea96e852ffc86eff41b98714f45
kevinmsmith131 <kevinmsmith131@gmail.com>
2021-06-28 14:16:46 -0400

    Fixed npm install bcrypt issue

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('./../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 
 // Route for registering a new user
 router.post('/register', async (request, response, next) => {
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('./../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 const mongoose = require('mongoose');
 
 // Get a user
