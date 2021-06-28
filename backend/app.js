ce91a50960f683c48961042929df78eafef5354f
kevinmsmith131 <kevinmsmith131@gmail.com>
2021-06-28 17:13:41 -0400

    Trying different method

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,9 @@ app.use(express.json());
 app.use(morgan('common'));
 
 // Set up app to handle request
-app.use(express.static('build'));
+app.get('*',(request, response) => {
+  response.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Handle files uploaded to the server
 const storage = multer.diskStorage({
