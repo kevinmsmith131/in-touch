e6ac10802d842f5a7c869fb4889690871b7c057b
kevinmsmith131 <kevinmsmith131@gmail.com>
2021-06-29 14:08:47 -0400

    Fixed image uploading with multer

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use('/users', usersRouter);
 // Create storage for multer
 const storage = multer.diskStorage({
   destination: (request, file, callback) => {
-    callback(null, 'build/images');
+    callback(null, 'build/images/post');
   }, 
   filename: (request, file, callback) => {
     callback(null, request.body.name);
